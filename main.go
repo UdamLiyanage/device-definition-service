930e7d9f4fb30d16cdf7ff19ed4717cabd5980cc
Udam Liyanage <udam1998@gmail.com>
2020-01-14 12:00:40 +0530

    Gin router setup and server ping

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Ping Successful",
+		})
+	})
+
+	return r
+}
+
 func main() {
-	println("Main Function")
+	r := setupRouter()
+	log.Fatal(r.Run())
 }
