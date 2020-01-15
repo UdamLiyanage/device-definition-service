4800c19743906f49b8dc8c9f06be168facdc43fd
Udam Liyanage <udam1998@gmail.com>
2020-01-15 16:53:23 +0530

    Changed route for create operation to avoid confusion

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func setupRouter() *gin.Engine {
 	})
 	r.GET("/device-definitions/:id", readDefinition)
 
-	r.POST("/devices-definitions", createDefinition)
+	r.POST("/devices-definitions/create", createDefinition)
 
 	r.PUT("/device-definitions/:id", updateDefinition)
 
